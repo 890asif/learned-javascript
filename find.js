741c14b47a9eaa7c7fc4be1523f58e18adcd9092
890asif <192508892+890asif@users.noreply.github.com>
2026-07-25 17:27:37 +0600

    find the largest of two numbers

---
+++ find.js
@@ -0,0 +1,13 @@
+// Find the largest of two numbers.
+
+let num1 = 349;
+let num2 = 233;
+if(num1 > num2){
+    console.log("number1 is bigger then number2 .")
+}
+else if(num2 > num1){
+    console.log("number2 is bigger them number1 .")
+}
+else{
+    console.log("Both number is equal.")
+}
