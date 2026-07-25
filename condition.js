dcee37c2046940f549b6e3c4ee8fd0279f832f83
890asif <192508892+890asif@users.noreply.github.com>
2026-07-25 17:01:21 +0600

    check a number a positive negetive or zero using if condition

---
+++ condition.js
@@ -0,0 +1,10 @@
+// check a number a positive, negetive or zero. 
+let number = 0; 
+if(number > 0){     
+    console.log('Number is positive.')
+} 
+ else if(number < 0){     
+    console.log('Number is Negetive.')
+} else{     
+    console.log('Number is Zero.') 
+}
